d8244554eac54b90d109b30c12b51a6126c03282
Fogotcheck <Fogotcheck@live.com>
2023-10-08 16:05:29 +0300

    add settings for CmakeLists
    Добавлены настройки для сборки платформы через cmake.

--- main/main.c
+++ main/main.c
@@ -1,11 +1,13 @@
 #include "main.h"
-
+#include "at32f435_437_clock.h"
 int main(void)
 {
+    system_clock_config();
+
     while (1)
     {
         /* code */
     }
-    
+
     return 0;
 }
--- main/main.h
+++ main/main.h
@@ -1,4 +1,4 @@
 #ifndef __Main_h__
 #define __Main_h__
-#include "../mztaSystemCallStubs/NewLibCallStabs.h"
+
 #endif //__Main_h__
